714b271a892295fbf4b2d5c51460c60ba2e0e6af
HazeNebula <jasper.opti@gmail.com>
2018-06-22 09:21:08 +0200

    Removed superfluous minimum width.

--- src/com/hazenebula/oubliette/ToolPane.java
+++ src/com/hazenebula/oubliette/ToolPane.java
@@ -2,23 +2,27 @@ package com.hazenebula.oubliette;
 
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
 
 public class ToolPane extends TabPane {
     private final Grid grid;
 
-    // todo: set minimum size in a better way
-    // todo: make toolpane resizable
     // todo: add wall object tab
+
     // todo: add object tab
+    // todo: add object resizing
+    // todo: add canvas to show resized image (on grid)
     public ToolPane(Grid grid) {
         setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+        setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, null,
+                null)));
 
         this.grid = grid;
 
         FieldColorPane fieldBrushes = new FieldColorPane();
         Tab fieldTab = new Tab("Field Brushes", fieldBrushes);
         getTabs().add(fieldTab);
-
-        setMinWidth(fieldBrushes.getMinWidth());
     }
 }
