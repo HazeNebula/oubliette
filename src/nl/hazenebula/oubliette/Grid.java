7223378883962ea16d984ced46b14679f5a718c4
HazeNebula <jasper.opti@gmail.com>
2018-06-22 11:34:08 +0200

    Added field drawing functionality to canvas.

--- src/nl/hazenebula/oubliette/Grid.java
+++ src/nl/hazenebula/oubliette/Grid.java
@@ -5,6 +5,7 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
 
 public class Grid extends ScrollPane {
     private static final int GRID_SIZE = 1;
@@ -18,18 +19,17 @@ public class Grid extends ScrollPane {
     private Field curField;
     private Brush curBrush;
 
-    // todo: add tool to draw with fields
     // todo: add tool to draw with wall objects
     // todo: add tool to draw with objects
     public Grid() {
         fieldGrid = new Field[100][100];
         for (int x = 0; x < fieldGrid.length; ++x) {
             for (int y = 0; y < fieldGrid[x].length; ++y) {
-                if (x % 2 == 0 && y % 2 != 0) {
-                    fieldGrid[x][y] = Field.FILLED;
-                } else {
+//                if (x % 2 == 0 && y % 2 != 0) {
+//                    fieldGrid[x][y] = Field.FILLED;
+//                } else {
                     fieldGrid[x][y] = Field.EMPTY;
-                }
+//                }
             }
         }
 
@@ -39,12 +39,35 @@ public class Grid extends ScrollPane {
                 fieldGrid[0].length * (size.get() + GRID_SIZE));
 
         setContent(canvas);
-        drawGrid();
+        drawFullGrid();
 
+        curBrush = Brush.FIELD;
         curField = Field.EMPTY;
+
+        canvas.addEventHandler(MouseEvent.ANY, new MouseDrawHandler(e -> {
+            if (curBrush == Brush.FIELD) {
+                int x = (int)(e.getX() / (size.get() + GRID_SIZE));
+                int y = (int)(e.getY() / (size.get() + GRID_SIZE));
+
+                fieldGrid[x][y] = curField;
+                drawField(x, y);
+            }
+        }));
     }
 
-    public void drawGrid() {
+    public void drawField(int x, int y) {
+        if (x >= 0 && x < fieldGrid.length
+                && y >= 0 && y < fieldGrid[y].length) {
+            double xPos = x * (size.get() + GRID_SIZE);
+            double yPos = y * (size.get() + GRID_SIZE);
+            GraphicsContext gc = canvas.getGraphicsContext2D();
+
+            gc.setFill(fieldGrid[x][y].color());
+            gc.fillRect(xPos + 1, yPos + 1, size.get(), size.get());
+        }
+    }
+
+    public void drawFullGrid() {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         gc.setFill(Field.FILLED.color());
---
+++ src/nl/hazenebula/oubliette/MouseDrawHandler.java
@@ -0,0 +1,27 @@
+package nl.hazenebula.oubliette;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+public class MouseDrawHandler implements EventHandler<MouseEvent> {
+    private final EventHandler<MouseEvent> onDrawEventHandler;
+
+    private boolean pressing = false;
+
+    public MouseDrawHandler(EventHandler<MouseEvent> onDrawEventHandler) {
+        this.onDrawEventHandler = onDrawEventHandler;
+    }
+
+    @Override
+    public void handle(MouseEvent e) {
+        if (e.getEventType() == MouseEvent.MOUSE_PRESSED) {
+            pressing = true;
+        } else if (e.getEventType() == MouseEvent.MOUSE_RELEASED) {
+            pressing = false;
+        }
+
+        if (pressing) {
+            onDrawEventHandler.handle(e);
+        }
+    }
+}
