071e21c39141591f0721435ab00bf2f5bc35f9d0
HazeNebula <jasper.opti@gmail.com>
2018-06-29 10:44:31 +0200

    Added labels that show PNG size.

--- src/nl/hazenebula/oubliette/MapBar.java
+++ src/nl/hazenebula/oubliette/MapBar.java
@@ -3,6 +3,7 @@ package nl.hazenebula.oubliette;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -19,6 +20,8 @@ public class MapBar extends GridPane {
         setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, null,
                 null)));
         setPadding(new Insets(5, 5, 5, 5));
+        setHgap(5);
+        setVgap(5);
 
         minimap = new Minimap(grid.snapshot());
         minimap.widthProperty().bind(widthProperty().subtract(
@@ -35,13 +38,30 @@ public class MapBar extends GridPane {
         }));
         timeline.playFromStart();
 
+        Label sizeLabel = new Label("Size:");
+
         Spinner<Integer> sizeField = new Spinner<>(Grid.MIN_SQUARE_SIZE,
                 Grid.MAX_SQUARE_SIZE, Grid.INIT_SQUARE_SIZE, 1);
         sizeField.setEditable(true);
         sizeField.valueProperty().addListener((observable, oldValue, newValue)
                 -> grid.setGridSize(newValue));
 
+        Label pngWidthLabel = new Label("PNG Width");
+        Label pngWidth = new Label();
+        pngWidth.textProperty().bind(grid.canvasWidthProperty().asString()
+                .concat(" px"));
+
+        Label pngHeightLabel = new Label("PNG Height");
+        Label pngHeight = new Label();
+        pngHeight.textProperty().bind(grid.canvasHeightProperty().asString()
+                .concat(" px"));
+
         add(minimap, 0, 0);
-        add(sizeField, 0, 1);
+        add(sizeLabel, 0, 1);
+        add(sizeField, 1, 1);
+        add(pngWidthLabel, 0, 2);
+        add(pngWidth, 1, 2);
+        add(pngHeightLabel, 0, 3);
+        add(pngHeight, 1, 3);
     }
 }
