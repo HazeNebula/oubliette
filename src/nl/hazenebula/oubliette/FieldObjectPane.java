720f0317a23d2b3cd49be76c73ad60b532f753a8
HazeNebula <jasper.opti@gmail.com>
2018-07-02 14:44:16 +0200

    Field objects can now be loaded from pane.

--- src/nl/hazenebula/oubliette/FieldObjectPane.java
+++ src/nl/hazenebula/oubliette/FieldObjectPane.java
@@ -6,10 +6,66 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class FieldObjectPane extends GridPane {
     private Canvas resizeCanvas;
 
+    private List<FieldObjectImage> objects;
     private int maxSize;
+
+    private void loadObjects() {
+        List<List<File>> files = ObjectFileSearcher.getObjects(
+                ObjectFileSearcher.FIELDOBJECT_DIR);
+        maxSize = 1;
+        objects = new LinkedList<>();
+
+        for (List<File> objectFiles : files) {
+            int size = (int)Math.sqrt(objectFiles.size());
+            FieldObjectImage img = new FieldObjectImage(size, size);
+
+            if (size > maxSize) {
+                maxSize = size;
+            }
+
+            for (File objFile : objectFiles) {
+                String filename = objFile.getName();
+                int lastHyphenIndex = filename.lastIndexOf('-');
+
+                if (lastHyphenIndex > -1) {
+                    String[] sizesStr = filename.substring(lastHyphenIndex + 1,
+                            filename.indexOf('.')).split("x");
+                    int[] sizes = new int[2];
+
+                    for (int i = 0; i < sizes.length; ++i) {
+                        try {
+                            sizes[i] = Integer.parseInt(sizesStr[i]);
+                        } catch (NumberFormatException e) {
+                            System.err.println("Could not format as number: " +
+                                    sizesStr[i]);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            System.err.println("Could not parse file name: " +
+                                    filename);
+                        }
+                    }
+
+                    try {
+                        img.setImage(sizes[0], sizes[1], objFile);
+                    } catch (IOException e) {
+                        System.err.println("Could not open file: " + objFile
+                                .toString());
+                    }
+                }
+            }
+
+            if (img.completed()) {
+                objects.add(img);
+            }
+        }
+    }
 
     public FieldObjectPane(Grid grid) {
         setAlignment(Pos.TOP_CENTER);
@@ -17,8 +73,7 @@ public class FieldObjectPane extends GridPane {
         setVgap(5.0d);
         setHgap(5.0d);
 
-        // highptodo: add file scanner
-        maxSize = 3;
+        loadObjects();
 
         resizeCanvas = new Canvas();
         resizeCanvas.widthProperty().bind(grid.sizeProperty()
