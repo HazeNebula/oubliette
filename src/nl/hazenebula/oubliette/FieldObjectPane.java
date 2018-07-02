ae345e97ac9949ae2d5d7ceee390c490f07b054c
HazeNebula <jasper.opti@gmail.com>
2018-07-02 15:18:31 +0200

    Added field object buttons.

--- src/nl/hazenebula/oubliette/FieldObjectPane.java
+++ src/nl/hazenebula/oubliette/FieldObjectPane.java
@@ -4,11 +4,14 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -16,6 +19,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class FieldObjectPane extends GridPane {
+    public static final double BUTTON_SIZE = 100.0d;
+
     private Canvas resizeCanvas;
 
     private List<FieldObjectImage> objects;
@@ -69,12 +74,7 @@ public class FieldObjectPane extends GridPane {
                         }
                     }
 
-                    try {
-                        img.setImage(sizes[0], sizes[1], objFile);
-                    } catch (IOException e) {
-                        System.err.println("Could not open file: " + objFile
-                                .toString());
-                    }
+                    img.setImage(sizes[0], sizes[1], objFile);
                 }
             }
 
@@ -108,14 +108,35 @@ public class FieldObjectPane extends GridPane {
         resizeWrapper.setPadding(new Insets(5, 5, 5, 5));
         resizeWrapper.add(resizeCanvas, 0, 0);
 
-        // highptodo: add buttons for field objects
+        // fixme: flowpane/scrollpane width should be equal to gridpane width
         // highptodo: make button press change field object brush
-        FlowPane buttonPane = new FlowPane();
+        ScrollPane buttonPane = new ScrollPane();
+
+        FlowPane buttons = new FlowPane();
+        buttons.setPadding(new Insets(5, 5, 5, 5));
+        setVgap(5);
+        setHgap(5);
+        setAlignment(Pos.TOP_CENTER);
+        buttonPane.setContent(buttons);
+
+        ToggleGroup fieldObjectToggleGroup = new ToggleGroup();
+        for (FieldObjectImage img : objects) {
+            ToggleButton button = new ToggleButton();
+
+            ImageView graphic = new ImageView(img.getImage(1, 1));
+            graphic.setFitWidth(BUTTON_SIZE);
+            graphic.setFitHeight(BUTTON_SIZE);
+            button.setGraphic(graphic);
+
+            button.setToggleGroup(fieldObjectToggleGroup);
+            buttons.getChildren().add(button);
+        }
 
         add(resizeWrapper, 0, 0, 2, 2);
         add(increaseSizeButton, 2, 0);
         add(decreaseSizeButton, 2, 1);
         add(rotateClockwiseButton, 0, 3);
         add(rotateAntiClockwiseButton, 1, 3);
+        add(buttonPane, 0, 4, 2, 1);
     }
 }
