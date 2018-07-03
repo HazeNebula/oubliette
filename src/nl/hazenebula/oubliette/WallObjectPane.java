405d49a5c12bf3ef8186d87b28275ed52c76012a
HazeNebula <jasper.opti@gmail.com>
2018-07-03 14:04:51 +0200

    Added nodes to wall object pane.

--- src/nl/hazenebula/oubliette/WallObjectPane.java
+++ src/nl/hazenebula/oubliette/WallObjectPane.java
@@ -1,8 +1,12 @@
 package nl.hazenebula.oubliette;
 
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 import java.io.File;
 import java.util.Arrays;
@@ -12,6 +16,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class WallObjectPane extends GridPane {
+    private static final double BUTTON_SIZE = 80.0d;
+
     private int maxSize;
 
     private Grid grid;
@@ -26,11 +32,140 @@ public class WallObjectPane extends GridPane {
         setVgap(5.0d);
         setHgap(5.0d);
 
-        List<WallObjectImage> images = loadObjects();
+        resizeCanvas = new Canvas();
+        resizeCanvas.widthProperty().bind(widthProperty().divide(2.0d));
+        resizeCanvas.heightProperty().bind(resizeCanvas.widthProperty()
+                .divide(2.0d));
+        resizeCanvas.widthProperty().addListener((observable, oldValue,
+                                                  newValue) -> drawCanvas());
+        GridPane.setHgrow(resizeCanvas, Priority.ALWAYS);
+        GridPane.setHalignment(resizeCanvas, HPos.CENTER);
 
-        // highptodo: add wall object preview
+        GridPane resizeWrapper = new GridPane();
+        resizeWrapper.add(resizeCanvas, 0, 0);
 
-        // highptodo: add wall object buttons
+        setHgrow(resizeWrapper, Priority.ALWAYS);
+        setHalignment(resizeWrapper, HPos.CENTER);
+
+        Label sizeLabel = new Label("Size:");
+        GridPane.setHgrow(sizeLabel, Priority.ALWAYS);
+        Button increaseSizeButton = new Button("+");
+        increaseSizeButton.setOnAction(e -> {
+            if (curObject.getWidth() < maxSize) {
+                curObject.setWidth(curObject.getWidth() + 1);
+                curObject.setImage(curImg.getImage(curObject.getWidth()));
+                drawCanvas();
+
+                grid.setFieldObject(curObject);
+            }
+        });
+        Button decreaseSizeButton = new Button("-");
+        decreaseSizeButton.setOnAction(e -> {
+            if (curObject.getWidth() > 1) {
+                curObject.setWidth(curObject.getWidth() - 1);
+                curObject.setImage(curImg.getImage(curObject.getWidth()));
+                drawCanvas();
+
+                grid.setFieldObject(curObject);
+            }
+        });
+        Label rotateLabel = new Label("Rotate:");
+        GridPane.setHgrow(rotateLabel, Priority.ALWAYS);
+        Button rotateClockwiseButton = new Button("\u21BB");
+        rotateClockwiseButton.setOnAction(e -> {
+            curObject.setDir(curObject.getDir().next());
+            drawCanvas();
+
+            grid.setFieldObject(curObject);
+        });
+        Button rotateCounterclockwiseButton = new Button("\u21BA");
+        rotateCounterclockwiseButton.setOnAction(e -> {
+            curObject.setDir(curObject.getDir().prev());
+            drawCanvas();
+
+            grid.setFieldObject(curObject);
+        });
+
+        GridPane shapeButtonPane = new GridPane();
+        shapeButtonPane.add(sizeLabel, 0, 0);
+        shapeButtonPane.add(increaseSizeButton, 1, 0);
+        shapeButtonPane.add(decreaseSizeButton, 2, 0);
+        shapeButtonPane.add(rotateClockwiseButton, 1, 1);
+        shapeButtonPane.add(rotateCounterclockwiseButton, 2, 1);
+
+        ToggleGroup toggleGroup = new ToggleGroup();
+
+        ToggleButton eraseButton = new ToggleButton("Erase");
+        eraseButton.setToggleGroup(toggleGroup);
+        eraseButton.setMaxWidth(Double.MAX_VALUE);
+        eraseButton.setOnAction(e -> {
+            grid.setBrush(Brush.FIELD_OBJECT_ERASE);
+        });
+        setHgrow(eraseButton, Priority.ALWAYS);
+        setHalignment(eraseButton, HPos.CENTER);
+
+        ScrollPane buttonPane = new ScrollPane();
+        buttonPane.setFitToWidth(true);
+        buttonPane.setStyle("-fx-background: #D3D3D3");
+
+        FlowPane buttons = new FlowPane();
+        buttons.setPadding(new Insets(5, 5, 5, 5));
+        setVgap(5);
+        setHgap(5);
+        buttons.setBackground(new Background(new BackgroundFill(
+                Color.LIGHTGRAY, null, null)));
+        buttonPane.setContent(buttons);
+
+        List<WallObjectImage> objects = loadObjects();
+
+        ToggleButton firstButton = new ToggleButton();
+        for (WallObjectImage img : objects) {
+            ToggleButton button = new ToggleButton();
+
+            if (img == objects.get(0)) {
+                firstButton = button;
+            }
+
+            ImageView graphic = new ImageView(img.getImage(1));
+            graphic.setFitWidth(BUTTON_SIZE);
+            graphic.setFitHeight(BUTTON_SIZE);
+            button.setGraphic(graphic);
+            button.setTooltip(new Tooltip(img.getName()));
+
+            button.setOnAction(e -> {
+                grid.setBrush(Brush.WALL_OBJECT);
+
+                if (curImg != img) {
+                    curImg = img;
+                    curObject = new FieldObject(curImg.getImage(
+                            curObject.getWidth()),
+                            curObject.getX(), curObject.getY(),
+                            curObject.getWidth(), curObject.getHeight(),
+                            curObject.getDir());
+                    drawCanvas();
+
+                    grid.setFieldObject(new FieldObject(curObject));
+                }
+            });
+
+            button.setToggleGroup(toggleGroup);
+            buttons.getChildren().add(button);
+        }
+
+        setVgrow(buttonPane, Priority.ALWAYS);
+
+        curImg = objects.get(0);
+        curObject = new FieldObject(curImg.getImage(1), 0, 0, 1, 1,
+                Direction.NORTH);
+        grid.setFieldObject(curObject);
+        firstButton.setSelected(true);
+
+        drawCanvas();
+
+        add(resizeWrapper, 0, 0);
+        add(shapeButtonPane, 0, 1);
+        add(eraseButton, 0, 2);
+        add(buttonPane, 0, 3);
     }
 
     private List<WallObjectImage> loadObjects() {
@@ -91,5 +226,9 @@ public class WallObjectPane extends GridPane {
         Collections.sort(objects);
 
         return objects;
+    }
+
+    private void drawCanvas() {
+        // highptodo: implement drawCanvas
     }
 }
