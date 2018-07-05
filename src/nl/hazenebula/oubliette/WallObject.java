0caa6187700b42808c153d2d1fe580c16dda77e2
HazeNebula <jasper.opti@gmail.com>
2018-07-05 13:26:56 +0200

    Added X and Y coordinates.

--- src/nl/hazenebula/oubliette/WallObject.java
+++ src/nl/hazenebula/oubliette/WallObject.java
@@ -4,17 +4,31 @@ import javafx.scene.image.Image;
 
 public class WallObject {
     private Image img;
+    private int x;
+    private int y;
     private int width;
     private Direction dir;
 
     public WallObject(Image img, int width, Direction dir) {
         this.img = img;
+        this.x = 0;
+        this.y = 0;
         this.width = width;
         this.dir = dir;
     }
 
     public WallObject(WallObject other) {
         img = other.img;
+        x = 0;
+        y = 0;
+        width = other.width;
+        dir = other.dir;
+    }
+
+    public WallObject(WallObject other, int x, int y) {
+        img = other.img;
+        this.x = x;
+        this.y = y;
         width = other.width;
         dir = other.dir;
     }
@@ -41,5 +55,13 @@ public class WallObject {
 
     public void setDir(Direction dir) {
         this.dir = dir;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
