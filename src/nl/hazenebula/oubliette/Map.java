b8388729f46fcdc1cdbfe77d8108cc2ab9ea9690
HazeNebula <jasper.opti@gmail.com>
2018-07-07 09:43:45 +0200

    Added resize method.

--- src/nl/hazenebula/oubliette/Map.java
+++ src/nl/hazenebula/oubliette/Map.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Map implements Serializable {
     private Field[][] fields;
-    private List<Object> objects;
+    private List<FieldObject> objects;
     private Wall[][][] walls;
 
     public Map(int width, int height, Field fill) {
@@ -20,6 +20,40 @@ public class Map implements Serializable {
         objects = new LinkedList<>();
 
         walls = new Wall[width + 1][height + 1][2];
+    }
+
+    public void resize(int width, int height, Field fill) {
+        Field[][] newFields = new Field[width][height];
+        for (int x = 0; x < newFields.length; ++x) {
+            for (int y = 0; y < newFields[x].length; ++y) {
+                if (x < fields.length && y < fields[x].length) {
+                    newFields[x][y] = fields[x][y];
+                } else {
+                    newFields[x][y] = fill;
+                }
+            }
+        }
+        fields = newFields;
+
+        List<FieldObject> newObjects = new LinkedList<>();
+        for (FieldObject obj : objects) {
+            if (obj.getX() >= 0 && obj.getX() + obj.getWidth() <= width
+                    && obj.getY() >= 0 && obj.getY() <= height) {
+                newObjects.add(obj);
+            }
+        }
+        objects = newObjects;
+
+        Wall[][][] newWalls = new Wall[width][height][2];
+        for (int x = 0; x < Math.min(walls.length, width); ++x) {
+            for (int y = 0; y < Math.min(walls.length, height); ++y) {
+                newWalls[x][y][Direction.NORTH.id()] = walls[x][y]
+                        [Direction.NORTH.id()];
+                newWalls[x][y][Direction.EAST.id()] = walls[x][y]
+                        [Direction.EAST.id()];
+            }
+        }
+        walls = newWalls;
     }
 
     public int getWidth() {
@@ -46,11 +80,11 @@ public class Map implements Serializable {
         this.fields = fields;
     }
 
-    public List<Object> getObjects() {
+    public List<FieldObject> getObjects() {
         return objects;
     }
 
-    public void setObjects(List<Object> objects) {
+    public void setObjects(List<FieldObject> objects) {
         this.objects = objects;
     }
 
