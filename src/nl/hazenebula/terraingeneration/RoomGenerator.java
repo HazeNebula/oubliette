3113c6d5e2a78e9b0fa65d013f87cc4500188954
HazeNebula <jasper.opti@gmail.com>
2018-07-08 16:19:19 +0200

    Added a room generating algorithm.

---
+++ src/nl/hazenebula/terraingeneration/RoomGenerator.java
@@ -0,0 +1,93 @@
+package nl.hazenebula.terraingeneration;
+
+import nl.hazenebula.oubliette.Field;
+import nl.hazenebula.oubliette.Map;
+
+public class RoomGenerator implements TerrainGenerator {
+    private final int numberOfAttempts;
+    private final int minWidth;
+    private final int maxWidth;
+    private final int minHeight;
+    private final int maxHeight;
+    private final Field floorTile;
+
+    private int curX;
+    private int curY;
+    private int curWidth;
+    private int curHeight;
+
+    public RoomGenerator(int numberOfAttempts, int minWidth, int maxWidth,
+                         int minHeight, int maxHeight, Field floorTile) {
+        this.numberOfAttempts = numberOfAttempts;
+        this.minWidth = minWidth;
+        this.maxWidth = maxWidth;
+        this.minHeight = minHeight;
+        this.maxHeight = maxHeight;
+        this.floorTile = floorTile;
+    }
+
+    private void getRoom(int width, int height) {
+        curWidth = Util.randInt(minWidth, maxWidth + 1);
+        curHeight = Util.randInt(minHeight, maxHeight + 1);
+        curX = Util.randInt(0, width - curWidth);
+        curY = Util.randInt(0, height - curHeight);
+    }
+
+    private boolean[][] initializeGrid(int xoffset, int yoffset, int width,
+                                       int height, Map map) {
+        boolean[][] grid = new boolean[width][height];
+
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                grid[x][y] = map.getField(x + xoffset, y + yoffset)
+                        == floorTile;
+            }
+        }
+
+        return grid;
+    }
+
+    private boolean roomFits(boolean[][] grid) {
+        for (int x = curX; x < curX + curWidth; ++x) {
+            for (int y = curY; y < curY + curHeight; ++y) {
+                if (grid[x][y]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private void placeRoomOnGrid(boolean[][] grid) {
+        for (int x = curX; x < curX + curWidth; ++x) {
+            for (int y = curY; y < curY + curHeight; ++y) {
+                grid[x][y] = true;
+            }
+        }
+    }
+
+    private void carveRooms(int xoffset, int yoffset, int width, int height,
+                            boolean[][] grid, Map map) {
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                map.setField(x + xoffset, y + yoffset, floorTile);
+            }
+        }
+    }
+
+    public Map generate(int x, int y, int width, int height, Map map) {
+        boolean[][] grid = initializeGrid(x, y, width, height, map);
+
+        for (int i = 0; i < numberOfAttempts; ++i) {
+            getRoom(width, height);
+            if (roomFits(grid)) {
+                placeRoomOnGrid(grid);
+            }
+        }
+
+        carveRooms(x, y, width, height, grid, map);
+
+        return map;
+    }
+}
