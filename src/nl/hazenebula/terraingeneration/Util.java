a3f037e06f802984dc4ecb02b2de4c18a5d768b3
HazeNebula <jasper.opti@gmail.com>
2018-07-08 17:44:01 +0200

    Added an if clause so randInt can be used if min == max.

--- src/nl/hazenebula/terraingeneration/Util.java
+++ src/nl/hazenebula/terraingeneration/Util.java
@@ -6,6 +6,10 @@ public class Util {
     private static final Random rand = new Random();
 
     public static int randInt(int min, int max) {
+        if (max == min) {
+            return 0;
+        }
+
         return rand.nextInt(max - min) + min;
     }
 
