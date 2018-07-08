aa3478f56cf9789e4bb3691496ab29b37e1224e4
HazeNebula <jasper.opti@gmail.com>
2018-07-08 14:17:54 +0200

    Added buttons for generators.

--- src/nl/hazenebula/oubliette/GeneratorPane.java
+++ src/nl/hazenebula/oubliette/GeneratorPane.java
@@ -1,20 +1,18 @@
 package nl.hazenebula.oubliette;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 
 public class GeneratorPane extends GridPane {
-    private CanvasPane canvasPane;
-
-    private Selection selection;
-
     public GeneratorPane(CanvasPane canvasPane) {
-        this.canvasPane = canvasPane;
-
-        selection = new Selection();
+        Selection selection = new Selection();
         canvasPane.setSelection(selection);
 
         Label selectionXLabel = new Label("Selection X:");
@@ -58,23 +56,58 @@ public class GeneratorPane extends GridPane {
             canvasPane.drawAll();
         });
 
-        // todo: add cave generator
-        // todo: add room generator
-        // todo: add maze generator
-        // todo: add room and maze generator
+        Label generatorLabel = new Label("Procedural Generators:");
+        GridPane.setHgrow(generatorLabel, Priority.ALWAYS);
+        generatorLabel.setPadding(new Insets(5, 5, 5, 5));
+
+        VBox buttonPane = new VBox();
+        buttonPane.setSpacing(5);
+
+        ToggleGroup toggleGroup = new ToggleGroup();
+
+        ToggleButton caveGeneratorButton = new ToggleButton("Cave Generator");
+        caveGeneratorButton.setMaxWidth(Double.MAX_VALUE);
+        caveGeneratorButton.setToggleGroup(toggleGroup);
+        caveGeneratorButton.setOnAction(e -> {
+            // todo: add cave generator
+        });
+        buttonPane.getChildren().add(caveGeneratorButton);
+
+        ToggleButton roomGeneratorButton = new ToggleButton("Room Generator");
+        roomGeneratorButton.setMaxWidth(Double.MAX_VALUE);
+        roomGeneratorButton.setToggleGroup(toggleGroup);
+        roomGeneratorButton.setOnAction(e -> {
+            // todo: add room generator
+        });
+        buttonPane.getChildren().add(roomGeneratorButton);
+
+        ToggleButton mazeGeneratorButton = new ToggleButton("Maze Generator");
+        mazeGeneratorButton.setMaxWidth(Double.MAX_VALUE);
+        mazeGeneratorButton.setToggleGroup(toggleGroup);
+        mazeGeneratorButton.setOnAction(e -> {
+            // todo: add maze generator
+        });
+        buttonPane.getChildren().add(mazeGeneratorButton);
+
+        ToggleButton compoundGeneratorButton = new ToggleButton("Compound " +
+                "Generator");
+        compoundGeneratorButton.setMaxWidth(Double.MAX_VALUE);
+        compoundGeneratorButton.setToggleGroup(toggleGroup);
+        compoundGeneratorButton.setOnAction(e -> {
+            // todo: add compound generator
+        });
+        buttonPane.getChildren().add(compoundGeneratorButton);
 
         add(selectionXLabel, 0, 0);
-//        add(xSpinner, 1, 0);
         add(selectionX, 1, 0);
         add(selectionYLabel, 0, 1);
-//        add(ySpinner, 1, 1);
         add(selectionY, 1, 1);
         add(selectionWidthLabel, 0, 2);
-//        add(widthSpinner, 1, 2);
         add(selectionWidth, 1, 2);
         add(selectionHeightLabel, 0, 3);
-//        add(heightSpinner, 1, 3);
         add(selectionHeight, 1, 3);
         add(selectAllButton, 0, 4, 2, 1);
+        add(generatorLabel, 0, 5, 2, 1);
+        add(buttonPane, 0, 6, 2, 1);
     }
 }
