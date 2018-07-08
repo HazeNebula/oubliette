7f0aa2e864e798cbb42f824b9fc95d3488aaf588
HazeNebula <jasper.opti@gmail.com>
2018-07-08 17:17:35 +0200

    generate() can now throw an IllegalArgumentException.

--- src/nl/hazenebula/terraingeneration/TerrainGenerator.java
+++ src/nl/hazenebula/terraingeneration/TerrainGenerator.java
@@ -3,5 +3,6 @@ package nl.hazenebula.terraingeneration;
 import nl.hazenebula.oubliette.Map;
 
 public interface TerrainGenerator {
-    Map generate(int x, int y, int width, int height, Map map);
+    Map generate(int x, int y, int width, int height, Map map) throws
+            IllegalArgumentException;
 }
