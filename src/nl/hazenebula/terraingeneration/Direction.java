3a12f627eafe1f627444523897e976a4072b297b
HazeNebula <jasper.opti@gmail.com>
2018-07-09 21:45:13 +0200

    Added a method that returns the opposite direction.

--- src/nl/hazenebula/terraingeneration/Direction.java
+++ src/nl/hazenebula/terraingeneration/Direction.java
@@ -21,4 +21,19 @@ public enum Direction {
     public int dy() {
         return dy;
     }
+
+    public Direction opposite() {
+        switch (this) {
+            case NORTH:
+                return SOUTH;
+            case EAST:
+                return WEST;
+            case SOUTH:
+                return NORTH;
+            case WEST:
+                return EAST;
+            default:
+                return null;
+        }
+    }
 }
